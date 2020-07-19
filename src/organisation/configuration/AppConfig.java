4d2737fce4cb69792b10c57f8ddb4c0ece6fb7c9
Bui The Cuong <buithecuong@gmail.com>
2020-07-19 10:49:17 -0400

    Update code

--- src/organisation/configuration/AppConfig.java
+++ src/organisation/configuration/AppConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import organisation.model.Employee;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EnableWebMvc
 @Configuration
